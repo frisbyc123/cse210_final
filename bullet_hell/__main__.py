94481709217138b6f660d485f81b83b945bd3ab8
Cameron <axxes9mc@gmail.com>
2021-11-27 21:59:50 -0700

    Trimmed up main

--- bullet_hell/__main__.py
+++ bullet_hell/__main__.py
@@ -1,11 +1,4 @@
 import os
-from game.constants import PADDLE_X, PADDLE_Y
-from game.constants import IMAGE_PADDLE, PADDLE_HEIGHT, PADDLE_WIDTH
-from game import handle_off_screen_action
-from game.constants import BALL_HEIGHT, BALL_WIDTH, IMAGE_BALL, MAX_X, MAX_Y
-from game.constants import BRICK_HEIGHT, BRICK_WIDTH, BRICKS_TALL, BRICKS_WIDE
-
-from game.constants import IMAGE_BRICK
 os.environ['RAYLIB_BIN_PATH'] = "."
 
 import random
@@ -20,9 +13,9 @@ from game.physics_service import PhysicsService
 from game.audio_service import AudioService
 
 # TODO: Add imports similar to the following when you create these classes
-from game.brick import Brick
-from game.ball import Ball
-from game.paddle import Paddle
+#from game.brick import Brick
+#from game.ball import Ball
+#from game.paddle import Paddle
 from game.control_actors_action import ControlActorsAction
 from game.handle_collisions_action import HandleCollisionsAction
 from game.handle_off_screen_action import HandleOffScreenAction
@@ -33,52 +26,41 @@ def main():
     # create the cast {key: tag, value: list}
     cast = {}
 
-    cast["bricks"] = []
-    # TODO: Create bricks here and add them to the list
-    bricks = []
-    for n in range(BRICKS_TALL):
-        for j in range(BRICKS_WIDE):
-   
-            x = 0 + (BRICK_WIDTH * j)
-            y = 0 + (BRICK_HEIGHT * n)
-            position = Point(x, y)
-            brick = Brick()
-            brick.set_image(IMAGE_BRICK)
-            brick.set_position(position)
-            bricks.append(brick)
-
-    cast["bricks"] = bricks
+    cast["enemy"] = []
+    # Create enemies here and add them to the list
+    enemies = []   
+    cast["enemy"] = enemies
 
     #for brick in cast["bricks"]:
     #    print(f"x position {brick._position.get_x()}")
     #    print(f"y position {brick._position.get_y()}")
 
-    cast["balls"] = []
-    # TODO: Create a ball here and add it to the list
-    balls = []
-    ball = Ball()
-    ball.set_image(IMAGE_BALL)
-    ball.set_width(BALL_WIDTH)
-    ball.set_height(BALL_HEIGHT)
-    x = MAX_X / 2
-    y = MAX_Y / 2
-    position = Point(x, y)
-    ball.set_position(position)
-    balls.append(ball)
-    cast["balls"] = balls
+    cast["bullet"] = []
+    # Create a bullets here and add it to the list
+    bullets = []
+    #ball = Ball()
+    #ball.set_image(IMAGE_BALL)
+    #ball.set_width(BALL_WIDTH)
+    #ball.set_height(BALL_HEIGHT)
+    #x = MAX_X / 2
+    #y = MAX_Y / 2
+    #position = Point(x, y)
+    #ball.set_position(position)
+    #balls.append(ball)
+    cast["bullet"] = bullets
     
-    cast["paddle"] = []
-    # TODO: Create a paddle here and add it to the list
+    cast["player_ship"] = []
+    # TODO: Create a player ship here and add it to the list
 
-    paddles = []
-    paddle = Paddle()
-    paddle.set_image(IMAGE_PADDLE)
-    paddle.set_width(PADDLE_WIDTH)
-    paddle.set_height(PADDLE_HEIGHT)
-    position = Point(PADDLE_X, PADDLE_Y)
-    paddle.set_position(position)
-    paddles.append(paddle)
-    cast["paddle"] = paddles
+    player_ships = []
+    #player_ship = player_ship()
+    #paddle.set_image(IMAGE_PADDLE)
+    #paddle.set_width(PADDLE_WIDTH)
+    #paddle.set_height(PADDLE_HEIGHT)
+    #position = Point(PADDLE_X, PADDLE_Y)
+    #paddle.set_position(position)
+    #paddles.append(paddle)
+    cast["player_ship"] = player_ships
 
 
     # Create the script {key: tag, value: list}
@@ -104,9 +86,9 @@ def main():
 
 
     # Start the game
-    output_service.open_window("Batter")
+    output_service.open_window("Bullet Hell")
     audio_service.start_audio()
-    audio_service.play_sound(constants.SOUND_START)
+    # audio_service.play_sound(constants.SOUND_START)
     
     director = Director(cast, script)
     director.start_game()
